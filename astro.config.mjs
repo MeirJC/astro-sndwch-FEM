2f4dcecd119f56fad97bca775daf306be5e4fa27
MeirJC <meir84@gmail.com>
2024-05-26 21:28:21 +0300

    updated config and netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
 import solidJs from '@astrojs/solid-js';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://astro-frontend-masters.netlify.app',
 	output: 'server',
 	integrations: [react(), solidJs()],
+	adapter: netlify(),
 });
